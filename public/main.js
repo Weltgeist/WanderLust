494682a41da391d0bd195cee705b3bb3f96be8af
Weltgeist <narcisse118@gmail.com>
2020-09-14 20:37:29 -0400

    turned getVenu into Async Fnction

--- public/main.js
+++ public/main.js
@@ -17,7 +17,7 @@ const $weatherDiv = $("#weather1");
 const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday'];
 
 // Add AJAX functions here:
-const getVenues = () => {
+const getVenues = async () => {
 
 }
 
