030e16309c2bedc8a8fec98e040c5257e706263b
Weltgeist <narcisse118@gmail.com>
2020-09-16 23:48:30 -0400

    added comments for debugging

--- public/main.js
+++ public/main.js
@@ -62,18 +62,18 @@ const renderVenues = (venues) => {
   $venueDivs.forEach(($venue, index) => {
     // Add your code here:
 
-    let venueContent = '';
-    const venue = venues[index];
-    const name = venue.name ;
-    const iconSource = venue.categories[0].icon ;
-    const venueImg = iconSource.prefix + 'bg_64' + iconSource.suffix ;//bg_64 comme from 4square API info.
-    const location = venue.location;
-    console.log(name);
-    console.log(venueImg);
-    console.log(location);
-    venueContent = createVenueHTML(name,location,venueImg);
-    console.log(venueContent)
-    $venue.append(venueContent);
+    //let venueContent = '';
+    //const venue = venues[index];
+    //const name = venue.name ;
+    //const iconSource = venue.categories[0].icon ;
+    //const venueImg = iconSource.prefix + 'bg_64' + iconSource.suffix ;//bg_64 comme from 4square API info.
+    //const location = venue.location;
+    //console.log(name);
+    //console.log(venueImg);
+    //console.log(location);
+    //venueContent = createVenueHTML(name,location,venueImg);
+    //console.log(venueContent)
+    //$venue.append(venueContent);
   });
   $destination.append(`<h2>${venues[0].location.city}</h2>`);
 }
